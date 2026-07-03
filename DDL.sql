1db7ee9e356708ef6ba30e20dd0fbeb0564a4dfc
johans-cid <freefire290n@gmail.com>
2026-07-02 22:11:20 -0400

    asd

--- DDL.sql
+++ DDL.sql
@@ -13,78 +13,33 @@ USE ventas;
 CREATE TABLE IF NOT EXISTS direcciones(
     -- Dentro de la tabla definimos campos, tipos de datos y 
     -- restricciones (Obligatoriedad de datos, claves primarias y foraneas)
-    id INTEGER NOT NULL, 
+    id_direccion INTEGER NOT NULL AUTO_INCREMENT,
     comuna VARCHAR(20) NOT NULL,
     calle VARCHAR(50) NULL,
-    numero VARCHAR(10) NULL DEFAULT "s/n",
+    numero VARCHAR(10) NULL DEFAULT 's/n',
     departamento VARCHAR(10) NULL,
 
-    CONSTRAINT pk_direcciones PRIMARY KEY (id)
+    CONSTRAINT pk_direcciones PRIMARY KEY (id_direccion)
 );
+
 CREATE TABLE IF NOT EXISTS empresas(
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id_empresas INTEGER NOT NULL AUTO_INCREMENT,
     razon_social VARCHAR(100) NOT NULL,
-    rut VARCHAR(12) NOT NULL UNIQUE,
+    rut VARCHAR(12) NULL UNIQUE,
     giro_comercial VARCHAR(255) NOT NULL,
     id_direcciones INTEGER NULL,
-    telefono_contacto VARCHAR(15) NULL,
-    correo_contacto VARCHAR(255) NULL,
-   
-    CONSTRAINT pk_empresas PRIMARY KEY (id),
-    CONSTRAINT fk_empresas_direcciones FOREIGN KEY (id_direcciones) REFERENCES direcciones(id)
-);
+    telefono_contacto VARCHAR(15) NOT NULL,
+    correo_contacto VARCHAR(255) NOT NULL,
 
-CREATE TABLE IF NOT EXISTS tipo_cliente(
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    tipo_cliente VARCHAR(20) NOT NULL,
-    descripcion_tipo_cliente VARCHAR(255) NULL,
-
-    CONSTRAINT pk_tipo_cliente PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS clientes(
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    nombre_ciente VARCHAR(100) NOT NULL,
-    rut_cliente VARCHAR(12) NOT NULL UNIQUE,
-    id_direccion_cliente INTEGER NULL,
-    telefono_cliente VARCHAR(15) NULL,
-    correo_cliente VARCHAR(255) NULL,
-    fecha_naicmiento DATE NOT NULL,
-    id_tipo_cliente INTEGER NOT NULL DEFAULT 1,
-
-    CONSTRAINT pk_clientes PRIMARY KEY (id),
-    CONSTRAINT fk_clientes_direcciones FOREIGN kEY (id_direccion_cliente) REFERENCES direcciones(id),
-    CONSTRAINT fk_clientes_tipos_clientes FOREIGN kEY (id_tipo_cliente) REFERENCES tipo_cliente(id)
-
+    CONSTRAINT pk_empresas PRIMARY KEY (id_empresas),
+    CONSTRAINT fk_empresas_direcciones FOREIGN KEY (id_direcciones) REFERENCES direcciones(id_direccion)
 
 );
 
-CREATE TABLE IF NOT EXISTS tipo_producto(
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    tipo_producto VARCHAR(255),
-    descricion_tipo_producto VARCHAR(255) NULL,
-
-    CONSTRAINT pk_tipos_productos PRIMARY KEY (id)
-
-
-);
-
-CREATE TABLE IF NOT EXISTS producto(
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    sku_producto VARCHAR(12) NOT NULL,
-    nombre_producto VARCHAR(255) NOT NULL,
-    id_tipo_producto INTEGER NOT NULL,
-    descripcion VARCHAR(255) NULL,
-    precio DECIMAL NOT NULL,
-    fechas_caducidad DATE NULL,
-
-    CONSTRAINT pk_productos PRIMARY KEY (id)
-    CONSTRAINT fk_productos_tipos_productos FOREIGN KEY (id_tipo_producto) REFERENCES tipo_producto(id)
-
-);
-
-CREATE TABLE IF NOT EXISTS inventario(
+ALTER TABLE empresas CHANGE id_empresas id_empresa INTEGER NOT NULL AUTO_INCREMENT;
+ALTER TABLE empresas CHANGE rut rut VARCHAR(12) NOT NULL UNIQUE;
+ALTER TABLE empresas CHANGE telefono_contacto telefono_contacto VARCHAR(15) NULL;
+ALTER TABLE empresas CHANGE correo_contacto correo_contacto VARCHAR(255) NULL;
 
 
 
-);
